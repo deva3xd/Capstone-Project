eefecd303f28cac6c6935e57a4e46f9e733ca02a
Raved <devarn05@gmail.com>
2023-11-24 23:02:12 +0700

    init

---
+++ resources/views/pelamar/pelamar.blade.php
@@ -0,0 +1,65 @@
+@extends('layouts.master-pelamar')
+
+@section('content')
+<section class="banner d-flex align-items-center">
+	<div class="banner-img-part"></div>
+	<div class="container">
+		<div class="row">
+			<div class="col-lg-10 col-md-12 col-xl-8">
+				<div class="block">
+					<span class="text-uppercase text-sm letter-spacing ">The most powerful Solution</span>
+					<h1 class="mb-3 mt-3">Digital experience with Orbitor</h1>
+					<p class="mb-5">Veritatis earum aliquid doloribus molestias, eveniet molestiae aperiam ratione. Facilis velit voluptatibus impedit eligendi delectus illo earum voluptatum, laudantium molestiae!</p>
+					<a href="about.html" target="_blank" class="btn btn-main">Learn more about Digicon<i class="fa fa-angle-right ml-2"></i></a>
+				</div>
+			</div>
+		</div>
+	</div>
+</section>
+<section class="section about">
+	<div class="container">
+		<div class="row">
+			<div class="col-lg-4 col-md-6 ">
+				<div class="about-item mb-5 mb-lg-5">
+					<div class="icon">
+						<i class="ti-light-bulb"></i>
+					</div>
+
+					<div class="content">
+						<h4 class="mt-3 mb-3">Custom Effective Software development</h4>
+						<p class="mb-4">Saepe nulla praesentium eaque omnis perferendis a doloremque.</p>
+
+						<a href="#"> Read More </a>
+					</div>
+				</div>
+			</div>
+
+			<div class="col-lg-4 col-md-6">
+				<div class="about-item mb-5 mb-lg-5">
+					<div class="icon">
+						<i class="ti-panel"></i>
+					</div>
+					<div class="content">
+						<h4 class="mt-3 mb-3">Analyse Your Expensees On Every Device</h4>
+						<p class="mb-4">Saepe nulla praesentium eaque omnis perferendis a doloremque.</p>
+						<a href="#"> Read More </a>
+					</div>
+				</div>
+			</div>
+			
+			<div class="col-lg-4 col-md-6">
+				<div class="about-item">
+					<div class="icon">
+						<i class="ti-headphone-alt"></i>
+					</div>
+					<div class="content">
+						<h4 class="mt-3 mb-3">Creating a dedicated IT business growth</h4>
+						<p class="mb-4">Saepe nulla praesentium eaque omnis perferendis a doloremque.</p>
+						<a href="#"> Read More </a>
+					</div>
+				</div>
+			</div>
+		</div>
+	</div>
+</section>
+@endsection
