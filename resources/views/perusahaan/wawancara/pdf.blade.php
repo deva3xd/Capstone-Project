2a6dd73da1a55dc9f9effdf6e16fc3b2284912aa
DESKTOP-V6RNP48\salma <salmanfarizi.as@gmail.com>
2023-12-04 20:17:28 +0700

    export pdf

--- resources/views/perusahaan/wawancara/pdf.blade.php
+++ resources/views/perusahaan/wawancara/pdf.blade.php
@@ -1,32 +1,35 @@
 <!DOCTYPE html>
-<html lang="en">
-
+<html>
 <head>
-    <meta charset="utf-8">
-    <meta http-equiv="X-UA-Compatible" content="IE=edge">
-    <meta name="viewport" content="width=device-width,initial-scale=1">
-    <title>Download PDF</title>
+	<title>Data Wawancara</title>
+	<link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css" integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">
 </head>
-<style type="text/css">
-	table tr td,
-	table tr th{
-		font-size: 9pt;
-	}
-</style>
-
 <body>
-    <table class="table table-striped table-bordered zero-configuration">
-        <thead>
-            <tr>
+	<style type="text/css">
+		table tr td,
+		table tr th{
+			font-size: 11pt;
+            border: 1px solid #999;
+            padding: 8px 20px;
+		}
+        
+	</style>
+	<center>
+		<h5>Data Wawancara</h4>
+	</center>
+ 
+	<table class='table table-bordered'>
+		<thead>
+			<tr>
                 <th>No.</th>
                 <th>ID Loker</th>
                 <th>ID Profile Pelamar</th>
                 <th>Jadwal</th>
                 <th>Catatan</th>
-            </tr>
-        </thead>
-        <tbody>
-            @foreach($wawancaras as $wawancara)
+			</tr>
+		</thead>
+		<tbody>
+			@foreach($wawancaras as $wawancara)
             <tr>
                 <td>{{ $loop->index + 1 }}</td>
                 <td>{{ $wawancara->id_loker }}</td>
@@ -35,7 +38,8 @@
                 <td>{{ $wawancara->catatan }}</td>
             </tr>
             @endforeach
-        </tbody>
-    </table>
+		</tbody>
+	</table>
+ 
 </body>
 </html>
