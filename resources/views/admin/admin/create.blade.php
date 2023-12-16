9e8a981fa2d1f4a60c708f618a9abaaa33f23184
DESKTOP-V6RNP48\salma <salmanfarizi.as@gmail.com>
2023-12-16 19:32:15 +0700

    Akun Admin Done

---
+++ resources/views/admin/admin/create.blade.php
@@ -0,0 +1,52 @@
+@extends('layouts.dashboard.master')
+@extends('layouts.dashboard.sidebar-admin')
+
+@section('content')
+    <!-- Content Header (Page header) -->
+    <div class="content-header">
+        <div class="container-fluid">
+            <div class="row mb-2">
+                <div class="col-sm-6">
+                    <h1 class="m-0 text-dark">Tambah Admin</h1>
+                </div>
+                <div class="col-sm-6">
+                </div>
+            </div>
+        </div>
+    </div>
+    <!-- /.content-header -->
+    
+    <!-- Content body start -->
+    <div class="container-fluid">
+        <div class="row">
+            <div class="col-12">
+                <div class="card">
+                    <div class="card-body">
+                        <div class="basic-form">
+                            <form action="{{route('storeAdmin')}}" method="POST">
+                                @csrf
+                                <div class="form-group row">
+                                    <label class="col-sm-2 col-form-label">Username</label>
+                                    <div class="col-sm-10">
+                                        <input type="text" class="form-control" placeholder="Username" name="username" id="username" required="required">
+                                    </div>
+                                </div>
+                                <div class="form-group row">
+                                    <label class="col-sm-2 col-form-label">Password</label>
+                                    <div class="col-sm-10">
+                                        <input type="text" class="form-control" placeholder="Password" name="password" id="password" required="required">
+                                    </div>
+                                </div>
+                                <div class="text-right">
+                                    <a href="#" class="btn btn-outline-secondary mr-2" role="button">Batal</a>
+                                    <button type="submit" class="btn btn-primary">Simpan</button>
+                                </div>
+                            </form>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+    <!-- Content body end -->
+@endsection
