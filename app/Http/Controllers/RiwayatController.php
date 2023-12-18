31d6a650a682ee8ab126c0229692484a29cce284
egarahmatul <egarahmatul0775@gmail.com>
2023-12-18 21:50:24 +0700

    create view riwayat lamaran

---
+++ app/Http/Controllers/RiwayatController.php
@@ -0,0 +1,11 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+
+class RiwayatController extends Controller
+{
+    // $wawancaras = Wawancara::all();
+    return view('pelamar.profil.riwayat');
+}
